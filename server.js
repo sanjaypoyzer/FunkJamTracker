56ecb3e3424c8f61fac8b992ab04ca9c55751328
Jon Buckley <jon@jbuckley.ca>
2013-10-27 19:14:21 +0000

    Allow content-type

--- server.js
+++ server.js
@@ -7,6 +7,7 @@ app.use(express.static(__dirname));
 app.use(express.directory(__dirname));
 app.use(function CORS(req, res, next) {
   res.set('Access-Control-Allow-Origin', '*');
+  res.set('Access-Control-Allow-Headers', 'Content-Type');
   next();
 });
 app.use(app.router);
